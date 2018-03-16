4f6b589a45431ef95ffe7a5b17c8d4f0e8084efc
Feng Yusheng <feng.yusheng@mw.mindwalkstudios.com>
2018-03-16 18:30:17 +0800

    Fucntion decorators and clousres

--- fluent_python/Chapter 7 Function decorators and closures/Fuction decorators and closures.md
+++ fluent_python/Chapter 7 Function decorators and closures/Fuction decorators and closures.md
@@ -40,5 +40,92 @@ clobber a global vairable without knowing.
 
 
 ## Closures
-A closure is a function with an extended scope. It can access non-global variables that are defined outside 
-of its body.
+A closure is a function with an extended scope. It can access non-global variables that are defined outside of its body.
+
+A closure is function that retains the bindings of the free variables that exist when the function is 
+defined, so that they can be used later when the function is invoked and the defining scope is no longer 
+available.
+
+**Note:** the only situation in which a function may need to deal with external variables that are non-global is when it is nested in another function.
+
+
+## The `nonlocal` declaration
+
+Example 7-13. A broken higher-order function to caculate a running average without keeping all history.
+Can you see where it breaks?
+
+```
+
+def make_averager():
+    count = 0
+    total = 0
+
+    def averager(new_value):
+        count += 1
+        total += new_value
+        return total / count
+
+    return averager
+```
+
+The objects stored in `__clousre__` are read-only, you can't change them. If you rebind them, 
+as in `count += 1`, you are implicitly creating a local variable count. It's no longer a free variable.
+
+To work around this rule the `nonlocal` declaration was introduced in Python 3.
+
+
+## Implementing a  simple decorator
+The typical behavior of a decorator is replacing the decorated funciton with a new funciton that accepts 
+the same arguments and (usually) returns whatever the decorated function was supposed to return, 
+while also doing some extra processing.
+
+
+## Decorators in the standard library
+Python has three built-in functions that are designed to decorate methods: 
+* `property`
+* `classmethod`
+* `staticmethod
+
+Trhe frequently seen decorators in standard library are:
+* `functools.wraps`
+* `functools.lru_cache`
+* `functools.singledispatch`
+
+**LRU** stands for *Least Recently Used*, meaning that the growth of the cache is limited 
+by discarding the entries that have not been read for a while.
+
+
+**generic funciton**
+A group of functions to perform the same operation in different ways, depending on the type of the first argument.
+This is what is meant by the term single-dispatch. If more arguments were used to select the specific function, we'd 
+have multiple-dispatch.
+
+https://docs.python.org/3/glossary.html#term-generic-function
+
+>A funciton composed of multiple functions implementing the same operation for diffrent types. Which implementation 
+should be used during a call is determined by the dispatch algorithm.
+
+**singel dispatch**
+https://docs.python.org/3/glossary.html#term-single-dispatch
+
+>A form of generic function dispatch where the implementation is chosen based on the type of a single argument.
+
+Python doesn't have method or function overloading.
+
+When possible, reigster the specialized functions to handle ABCs (abstract classes) such as `numbers.Intergral` and 
+`abc.MutableSequence` instead of concrete implementation like `int` and `list`. **This allows your code to support a 
+greater variety of compatible types.** For example, a Python extension can provide alternatives to the `int` type 
+with fixed bit lengths as subclasses of `numbers.Integral.`
+
+
+## Statcked decorators
+```
+@d1
+@d2
+def f():
+    pass
+
+f = d1(d2(f()))
+```
+
+## Parametrized Decorators
--- fluent_python/Chapter 7 Function decorators and closures/code.py
+++ fluent_python/Chapter 7 Function decorators and closures/code.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 # Example 7-1. A decorator ususally replaces a function with a
 # different one.
 
@@ -60,6 +59,190 @@ def main():
     f3()
 
 
+# Compute the mean of an ever-increasing series of values.
+# Example 7-8. A class-based implementation.
+class Average():
+    def __init__(self):
+        self.series = []
+
+
+    def __call__(self, value):
+        self.series.append(value)
+        total = sum(self.series)
+        return total / len(self.series)
+
+
+# Example 7-9. A functional implementation, using a higher order funciton .
+# The binding for `series` is kept in the `__closure__` attribute of the returned 
+# function `arg`. Each item in `avg.__closure__` corresponds to a name in `avg.__code__.co_freevars`.
+# These items are cells, and they have an attribute cell_contents where the actual value can be found.
+def make_averager():
+    series = []
+
+    def averager(new_average):
+        series.append(new_average)
+        total = sum(series)
+        return total/len(series)
+
+    return averager
+
+
+# Example 7-14. Calcualte a running average without keeping all history. Fixed with 
+# the use of nonlocal
+def make_averager2():
+    count = 0
+    total = 0
+
+    def averager(new_average):
+        nonlocal count, total
+        count += 1
+        total += new_average
+        return total / count
+
+    return averager
+
+
+# Exapmle 7-15. A simple decorator to output the running time of functions.
+import time
+
+def clock(func):
+    def clocked(*args):
+        t0 = time.perf_counter()
+        result = func(*args)
+        elapsed = time.perf_counter() - t0
+        name = func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        print('[%0.8fs] %s(%s) -> %r' %(elapsed, name, arg_str, result))
+        return result
+    return clocked
+
+
+# Example 7-16. A simple decorator to output the running time of functions.
+@clock
+def snooze(seconds):
+    time.sleep(seconds)
+
+
+@clock
+def factorial(n):
+    return 1 if n < 2 else n*factorial(n-1)
+
+
+import functools
+
+def clock2(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, *kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_list = []
+        if args:
+            arg_list.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_list.append(', '.join(pairs))
+        arg_str = ', '.join(arg_list)
+        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
+
+
+# Exapmle 7-18. The very costly recusive way to compute the Nth number in the Fibonacci series.
+# fibonacci(1) is called 8 times, fibonacci(2) 5 time.
+
+
+@clock
+def fibonacci(n):
+    """
+                        6
+
+                  /            \\
+                 4              5
+               /  \\        /       \\
+              2     3      3         4 
+             / \\  / \\   / \\     /  \\
+            0   1 1   2  1   2    2     3
+                     / \\   / \\ / \\  / \\
+                    0   1  0   1 0  1 1   2
+                                        /   \\
+                                        0    1
+
+    """
+    if n < 2:
+        return n
+    return fibonacci(n-2) + fibonacci(n-1)
+
+
+# Example 7-19. Faster implementation using caching. This is an example of stacked decorators.
+# @lru_cache() is applied on the function returned by @clock.
+
+@functools.lru_cache()
+@clock
+def fibonacci2(n):
+    if n < 2:
+        return n
+    return fibonacci2(n-2) + fibonacci2(n-1)
+
+
+# Example 7-21. singledispatch creates a custom htmlize.resgister to bundle servral functions into 
+# a generic function.
+
+from collections import abc
+import numbers
+import html
+
+@functools.singledispatch
+def htmlize(obj):
+    content = html.escape(repr(obj))
+    return '<pre>{}</pre>'.format(content)
+
+@htmlize.register(str)
+def _(text):
+    content = html.escape(text).replace('\n', '<br>\n')
+    return '<p>{0}</p>'.format(content)
+
+@htmlize.register(numbers.Integral)
+def _(n):
+    return '<pre>{0} (0x{0:x})</pre>'.format(n)
+
+@htmlize.register(tuple)
+@htmlize.register(abc.MutableSequence)
+def _(seq):
+    inner = '</li>\n<li>'.join(htmlize(item) for item in seq)
+    return '<ul>\n<li>' + inner + '</li>\n</ul>'
+
+
+
+
+
 if __name__ == '__main__':
     # target()
-    main()
+
+    # main()
+
+    # avg = Average() 
+    # print(avg(10))
+    # print(avg(11))
+    # print(avg(12))
+
+    # avg = make_averager()
+    # avg = make_averager2()
+    # print(avg(10))
+    # print(avg(11))
+    # print(avg(12))
+    
+    # print(avg.__code__.co_freevars)
+    # print(avg.__closure__)
+    # print(avg.__closure__[0].cell_contents)
+
+    # print('*'*40, 'Calling snooze(.123)')
+    # snooze(.123)
+    # print('*'*40, 'Calling factorial(6)')
+    # print('6! = ', factorial(6))
+
+    # print(fibonacci(6))
+
+    print(fibonacci2(6))
+    print(fibonacci2.cache_info())
